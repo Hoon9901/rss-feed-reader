e3eed413563ac960e5bca97ebac1430c675b2550
Hoon9901 <shonn.dev@gmail.com>
2023-04-05 16:40:19 +0900

    feat: 명령어 추가 및 기능 개선

--- bot.py
+++ bot.py
@@ -8,11 +8,19 @@ from updater import start
 intents = Intents.default()
 intents.messages = True
 
-
 class RSSBot(Bot):
+    time = "300" # defalt 5 minutes
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.updates_scheduled = False
+    
+    def updateTime(self, time):
+        self.time = time
+        print(f"Updated time to {time} seconds.")
+
+    def getTime(self):
+        return self.time
 
 
 client = RSSBot(intents=intents, command_prefix="rss!")
@@ -21,7 +29,8 @@ client = RSSBot(intents=intents, command_prefix="rss!")
 @client.listen()
 async def on_ready():
     if not client.updates_scheduled:
-        start(client.loop, db, client)
+        print(f"Starting the feed update loop {client.getTime()} seconds")
+        start(client.getTime(), client.loop, db, client)
 
     print("We're alive!!!")
 
@@ -35,7 +44,7 @@ async def add(ctx: Context, feed_url: str):
     guilds[str(ctx.guild.id)] = feeds
     db.set("feeds", guilds)
 
-    await ctx.send(f"피드 목록에  `{feed_url}` 추가됨! [{ctx.guild.name}]")
+    await ctx.send(f"[{ctx.guild.name}] 피드 목록에  `{feed_url}` 추가됨! ")
 
 
 @client.command(name="삭제")
@@ -70,19 +79,44 @@ async def setup(ctx: Context, channel: TextChannel):
     await ctx.send(f"[{ctx.guild.name}] {channel.mention} RSS 피드 채널로 성공적으로 설정되었습니다.")
 
 
-@client.command(name="정보")
+@client.command(name="채널정보")
 async def info(ctx: Context):
     channel_id = db.get("guilds", default={}).get(str(ctx.guild.id), None)
     channel = ctx.guild.get_channel(channel_id).mention if channel_id is not None else "*Not Set*"
     await ctx.send(f"[{ctx.guild.name}] RSS 피드 채널은 이곳 -> {channel} ")
 
+@client.command(name="갱신정보")
+async def info(ctx: Context):
+    await ctx.send(f"[{ctx.guild.name}] RSS 피드 갱신 주기 : {client.getTime()} ")
+
+
+@client.command(name="갱신설정")
+async def info(ctx: Context, time: str):
+    old_time = client.getTime()
+    client.updateTime(time)
+    await ctx.send(f"[{ctx.guild.name}] RSS 피드 갱신 주기 변경! -> {old_time} 에서 {client.getTime()} ")
+
+@client.command(name="봇정보")
+async def info(ctx: Context):
+    channel_id = db.get("guilds", default={}).get(str(ctx.guild.id), None)
+    channel = ctx.guild.get_channel(channel_id).mention if channel_id is not None else "*Not Set*"
+    await ctx.send(f"**RSS 피드 Reader 봇 정보입니다.**\n"
+    + "RSS 피드 채널 : " + channel + "\n"
+    + "RSS 갱신 주기 : " + client.getTime() + "초\n"
+    + "created by @hoon9901")
+
+
 @client.command(name="명령어")
 async def info(ctx: Context):
-    await ctx.send(f"rss!추가 <피드 URL> - 피드를 추가합니다.\n" 
+    await ctx.send(f"**RSS 피드 Reader 봇 명령어 목록입니다.**\n"
+    + "rss!추가 <피드 URL> - 피드를 추가합니다.\n" 
     + "rss!삭제 <피드 URL> - 피드를 삭제합니다.\n" 
     + "rss!목록 - 피드 목록을 보여줍니다.\n" 
     + "rss!채널설정 <채널> - RSS 피드 채널을 설정합니다.\n" 
-    + "rss!정보 - RSS 피드 채널 정보를 보여줍니다.\n" 
+    + "rss!채널정보 - RSS 피드 채널 정보를 보여줍니다.\n" 
+    + "rss!갱신정보 - RSS 피드 갱신 주기를 보여줍니다.\n"
+    + "rss!갱신설정 <초단위> - RSS 피드 갱신 주기를 설정합니다.\n"
+    + "rss!봇정보 - 봇 정보를 보여줍니다.\n"
     + "rss!명령어 - 명령어를 보여줍니다.")
 
 
